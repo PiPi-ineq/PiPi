fc688cb07bfc63864cc58b4a3c52d01a829f43ae
Gergely Tóth <toth.gergo@gmail.com>
2020-03-20 15:02:50 +0000

    Update pipi.R add new return value

--- R/pipi.R
+++ R/pipi.R
@@ -1,4 +1,3 @@
-
 #' Calculate the PiPi Inequality Index
 #'
 #' @aliases pipi
@@ -75,6 +74,7 @@ PiPi.Test <- function(dta=NULL, ns=100, prev=NULL, .progress=TRUE, prob=.95, ...
     prev$StrMin0 <- c(prev$StrMin0, cache$StrMin)
   }
   if (.progress) {close(pb) }
+  prev$prob <- prob
   prev$PiPi0.mean <- mean(prev$PiPi0)
   prev$PiPi0.median <- median(prev$PiPi0)
   q<- c((1-prob)/2, (1-(1-prob)/2))
